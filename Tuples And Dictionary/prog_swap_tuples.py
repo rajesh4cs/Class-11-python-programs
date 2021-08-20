eab93fb8ba066a1fd6915414e42e8aca17388d4f
Mayank Raj <89139319+mayankraj161s@users.noreply.github.com>
2021-08-20 17:36:49 +0530

    Update prog_swap_tuples.py

--- Tuples And Dictionary/prog_swap_tuples.py
+++ Tuples And Dictionary/prog_swap_tuples.py
@@ -1,12 +1,12 @@
 t1 = tuple()
 n = int(input("Total no. of values in first tuple: "))
 for i in range(n):
-	a = input("Enter elements: ")
+	a = input("Enter element no.{} : ".format(i+1))
 	t1 += (a,)
 t2 = tuple()
 m = int(input("Total no. of values in second tuple: "))
 for i in range(m):
-	a = input("Enter elements: ")
+	a = input("Enter element no.{} : ".format(i+1))
 	t2 += (a,)
 print("First tuple: ",t1)
 print("Second tuple: ",t2)
